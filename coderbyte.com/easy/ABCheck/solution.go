6d46eb9714f0a2aa4345f27cb8f69ae743eced3e
Olayemii <donyemisco@gmail.com>
2021-02-18 11:13:57 +0100

    setup first

---
+++ coderbyte.com/easy/ABCheck/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import "strings"
+
+/*
+Have the function ABCheck(str) take the str parameter being passed
+and return the string true if the characters a and b are separated
+by exactly 3 places anywhere in the string at least once
+(ie. "lane borrowed" would result in true because there is exactly three characters between a and b).
+Otherwise return the string false.
+*/
+
+func ABCheck(str string) bool {
+	for i, letter := range str {
+		if strings.ToLower(string(letter)) == "a" && strings.ToLower(string(str[i+4])) == "b" {
+			return true
+		}
+		if strings.ToLower(string(letter)) == "b" && strings.ToLower(string(str[i+4])) == "b" {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	ABCheck("olayemii")
+}
